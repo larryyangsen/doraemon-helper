5e87712cf419efa1a955530134be99db3a689737
Sen Hao Yang <larryyangsen@hotmail.com>
2019-08-03 16:36:55 +0800

    [fix] not to present 土

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import 'normalize.css';
 const itemMaps = {
-    0: '土',
+    0: '',
     1: '石',
     2: '銅',
     3: '鐵',
@@ -18,22 +18,23 @@ const itemMaps = {
     12: '鑽',
     13: '下'
 };
+
 const App = () => {
     const selectRef = useRef();
     const bedRef = useRef();
     const selectedIndex = useRef(0);
     const [bed, setBed] = useState(() => Array(49).fill(0));
-    const onClick = index => {
+    const onClick = (index, item) => {
         selectRef.current.style.opacity = 1;
         selectRef.current.focus();
         selectedIndex.current = index;
+        selectRef.current.selectedIndex = item;
     };
     const onItemChange = e => {
         const index = selectedIndex.current;
         const newBed = bed.slice();
         newBed[index] = +e.target.value;
         setBed(newBed);
-        selectRef.current.selectedIndex = '0';
         selectRef.current.style.opacity = 0;
         selectRef.current.blur();
         bedRef.current.focus();
@@ -43,9 +44,9 @@ const App = () => {
     return (
         <>
             <div ref={bedRef} className="bed">
-                {bed.map((a, i) => (
-                    <button onClick={() => onClick(i)} key={i}>
-                        <span>{itemMaps[a]}</span>
+                {bed.map((item, i) => (
+                    <button onClick={() => onClick(i, item)} key={i}>
+                        <span>{itemMaps[item]}</span>
                     </button>
                 ))}
             </div>
@@ -59,6 +60,8 @@ const App = () => {
                     ))}
                 </select>
             </div>
+            <p>化：化石類</p>
+            <p>下：下一層</p>
         </>
     );
 };
